82c93d73196a460f642cd7e5507d76b293dc6688
beshandyman <beshandymanfix@gmail.com>
2026-02-21 18:12:52 -0600

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Forces the creation of the static "out" folder
   output: 'export',
+  
+  // Required for GitHub Pages
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
