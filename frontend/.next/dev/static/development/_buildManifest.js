9690e78741f4a8eb8069b477cb29e4c0bbe93032
Brian van Vlymen <arduino731@gmail.com>
2026-03-13 23:23:22 -0500

    This is work docker and vite

---
+++ frontend/.next/dev/static/development/_buildManifest.js
@@ -0,0 +1,26 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/pages/index.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/CityHome",
+    "/GuestProfile",
+    "/HandymanProfile",
+    "/Home2",
+    "/Login",
+    "/NewPage",
+    "/Register",
+    "/Services",
+    "/SkillGallery",
+    "/TaskerOnboarding",
+    "/TaskerProfile",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
