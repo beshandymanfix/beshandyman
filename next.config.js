3072518ce4e6fdde63b9f72c9180b9b4fd80e42b
Brian van Vlymen <arduino731@gmail.com>
2026-03-22 23:25:13 -0500

    update Docker

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   // Removed to allow dynamic database routes like /handyman/[id] to work automatically
   // output: 'export',
-  
+  output: 'standalone', // enable shrink the size of your app for Docker 
   // Required for GitHub Pages
   images: {
     unoptimized: true,
